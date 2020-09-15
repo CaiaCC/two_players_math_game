48ed6fbc969f5f417cc1cf09c7dc659d5e68866f
Caia Chuang <caiachung@gmail.com>
2020-09-15 17:29:11 +0000

    Add question.rb and modify player.rb.

--- player.rb
+++ player.rb
@@ -1,3 +1,5 @@
+require './question'
+
 class Player
   attr_reader :name
   attr_accessor :lives
@@ -7,11 +9,22 @@ class Player
     @lives = 3
   end
   
-  def lose_lives(answer_correct?)
-    answer_correct? ? lives -=0  : lives -= 1
+  def current_lives(boolean)
+    boolean ? lives : @lives -= 1  # lives -= 1 won't work if no @ prepend
   end
 
   def dead?
     lives <= 0 ? true : false
   end
 end
+
+# caia = Player.new("Caia")
+# print caia.current_lives(false)
+# p caia
+# caia.current_lives(false)
+# p caia
+# caia.current_lives(true)
+# p caia
+# caia.current_lives(false)
+# p caia
+# p caia.dead?
---
+++ question.rb
@@ -0,0 +1,29 @@
+class Question
+  attr_reader :number1, :number2, :math_problem, :correct_answer
+  attr_accessor :palyer_answer
+
+  def initialize
+    @number1 = rand(1..20)
+    @number2 = rand(1..20)
+    @correct_answer = number1 + number2
+    @math_problem = "What does #{number1} plus #{number2} equal?"
+    puts math_problem
+    @palyer_answer = gets.chomp.to_i
+  end
+  
+  def answer_correct?
+    palyer_answer == correct_answer
+  end
+  
+  def feedback
+    if self.answer_correct? 
+      puts "Yes! You are correct." 
+    else 
+      puts "Seriously? No!"
+    end
+  end
+end
+
+# Q1 = Question.new
+# puts Q1.answer_correct?
+# puts Q1.feedback
